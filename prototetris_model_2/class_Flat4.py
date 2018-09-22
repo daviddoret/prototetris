d08f83a50545480e38bab43f2d4251b253a3d039
Heloise <heloise.doret@gmail.com>
2018-09-22 14:42:29 +0200

    test

---
+++ prototetris_model_2/class_Flat4.py
@@ -0,0 +1,23 @@
+from prototetris_model_2.class_Flat import Flat
+from sympy import *
+
+
+class Flat2(Flat):
+    def __init__(
+            self,
+            label=None,
+            circumscribed_shape=None,
+            position=Point3D(0, 0, 0),
+            *args,
+            **kwargs):
+
+        Flat.__init__(
+            self,
+            label=label,
+            description="4 rooms flat",
+            circumscribed_shape=circumscribed_shape,
+            position=position,
+            polygon_base=Polygon((0, 0), (12500, 0), (12500, 600), (0, 600), (0, 0)),
+            height=300,  # TODO: WIll be linked to the parent floor's height
+            *args,
+            **kwargs)
